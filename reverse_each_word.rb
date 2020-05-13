c6e748bce5adbbfa28f80b45bb5f9ac01720324f
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-05-13 23:14:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence1)
   sentence1 = "Hello there, and how are you?"
   array = "Hello there, and how are you?".split
   array.each do |words|
-   puts ""
+   word.reverse
 end
 
 end  
